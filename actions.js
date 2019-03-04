06d76747112e8efb7935d362820b6c6c59ca744c
shakil <36266736+sXakil@users.noreply.github.com>
2019-03-04 23:52:24 +0600

    key added with shortid

--- actions.js
+++ actions.js
@@ -1,14 +1,21 @@
+import shortid from 'shortid'
 // action types
 export const UPDATE_USER = 'UPDATE_USER'
 export const UPDATE_CONTACT = 'UPDATE_CONTACT'
 
 // action creators
-export const updateUser = update => ({
-  type: UPDATE_USER,
-  payload: update,
-})
+export const updateUser = payload => {
+  payload.key = shortid.generate()
+  return {
+    type: UPDATE_CONTACT,
+    payload,
+  }
+}
 
-export const addContact = newContact => ({
-  type: UPDATE_CONTACT,
-  payload: newContact,
-})
+export const addContact = payload => {
+  payload.key = shortid.generate()
+  return {
+    type: UPDATE_CONTACT,
+    payload,
+  }
+}
--- store.js
+++ store.js
@@ -5,9 +5,9 @@ import reducers from './reducers'
 const store = createStore(reducers)
 
 /* seed initial data */
-store.dispatch(addContact({name: 'Shakil Ahmed', phone: '5446521956'}))
-store.dispatch(addContact({name: 'Jordan Hayashi', phone: '5328315496'}))
-store.dispatch(addContact({name: 'David Cameroon', phone: '5050505050'}))
+store.dispatch(addContact({name: 'Shakil Ahmed', phone: '5446521956',}))
+store.dispatch(addContact({name: 'Jordan Hayashi', phone: '5328315496',}))
+store.dispatch(addContact({name: 'David Cameroon', phone: '5050505050',}))
 console.log(store.getState())
 
 export default store
